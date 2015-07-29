b9b7943b90bc26801ca1d315738d0b1720f8a20e
laughin1 <jasteph@uw.edu>
2015-07-29 16:51:11 -0700

    Init Project Setup

---
+++ SportsStore.UnitTests/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SportsStore.UnitTests
+{
+    public class Class1
+    {
+        [Fact]
+        public void FirstTest()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void SecondTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
